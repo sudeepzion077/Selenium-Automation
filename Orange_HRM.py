b55abf5fb86d1fd855dfb0017a6ed9842d09067c
python <sudeepzion077@gmail.com>
2023-02-02 16:06:41 +0530

    ORange

--- Orange_HRM.py
+++ Orange_HRM.py
@@ -1,5 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriveManager
 
-driver = webdriver.Chrome(executable_path="C:\drivers\chromedriver")
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach",True)
+driver = webdriver.Chrome(options=options)
+# driver.implicitly_wait(40)
 
 driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+# driver.find_element("username").send_keys("Admin")
+# driver.find_element("password").send_keys("admin123")
+# driver.find_element("submit").click()
+#
+# actual_key = driver.title
+# expected_key = "OrangeHRM"
+#
+# if actual_key == expected_key:
+#     print("Login test is passed")
+#
+# else:
+#     print("Login test is failed")
