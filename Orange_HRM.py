94f82f5391a747aab10352494279f242db374ac2
python <sudeepzion077@gmail.com>
2023-02-02 17:10:45 +0530

    ORange

--- Orange_HRM.py
+++ Orange_HRM.py
@@ -1,17 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriveManager
+from selenium.webdriver.common.by import By
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach",True)
 driver = webdriver.Chrome(options=options)
 driver.maximize_window()
 
-driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-# driver.find_element("username").send_keys("Admin")
-# driver.find_element("password").send_keys("admin123")
-# driver.find_element("submit").click()
-#
+driver.get("http://dev.financebox.in/auth/login")
+driver.find_element(By.NAME, "email").send_keys("Test@gmail.com")
+driver.find_element(By.NAME, "password").send_keys("3VE%5b9R9Y")
+driver.find_element(By.NAME, "Submit").click()
+
 # actual_key = driver.title
 # expected_key = "OrangeHRM"
 #
